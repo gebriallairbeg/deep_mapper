e0e7a0d6ab03788bf02639ce9ae1ca4ccba0b820
pavel-sherepo <gebrial@icloud.com>
2017-09-07 21:48:38 +0300

    gitignore, docs & setup as module preparation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='deep_mapper',
+      version='0.1',
+      description='Mapper that able to return an object with converted keys, or a list of objects',
+      url='http://github.com/gebriallairbeg/deep_mapper',
+      author='Pavel Sherepo',
+      author_email='pavel.sherepo@gmail.com',
+      license='MIT',
+      packages=['deep_mapper'],
+      zip_safe=False)
